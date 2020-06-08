1b5344b4d608dd337fb0715e026e0333c997ecd9
Sahan Bulathwela <sahabull@gmail.com>
2020-06-08 05:34:36 +0100

    feat: added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='context-agnostic-engagement',
+    version='1.0',
+    packages=['context_agnostic_engagement', 'context_agnostic_engagement.utils'],
+    url='www.x5gon.org',
+    license='',
+    author='Sahan Bulathwela',
+    author_email='m.bulathwela@ucl.ac.uk',
+    description='This python package includes the datasets and the helpfer functions that allow building models for predicting context-agnostic (population-based) of video lectures. '
+)
