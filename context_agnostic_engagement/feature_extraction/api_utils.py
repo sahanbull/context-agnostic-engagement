3ab33a552b9023030d1395867ae06ff6c53c9dfc
Sahan Bulathwela <sahabull@gmail.com>
2020-06-08 09:02:08 +0100

    feat: add wiki based features

---
+++ context_agnostic_engagement/feature_extraction/api_utils.py
@@ -0,0 +1,60 @@
+import time
+
+import requests
+import ujson as json
+
+ERROR_KEY = u'error'
+STATUS_FIELD = u'status'
+
+URL_FIELD = u'url'
+PAGERANK_FIELD = u'pageRank'
+
+COSINE_FIELD = u'cosine'
+
+ANNOTATION_DATA_FIELD = u'annotation_data'
+
+_WIKIFIER_URL = u"http://www.wikifier.org/annotate-article"
+_WIKIFIER_MAX_SERVER_LIMIT = 25000
+WIKIFIER_MAX_CHAR_CEILING = round(_WIKIFIER_MAX_SERVER_LIMIT * .99)  # 99% of max allowed num chars for a post request
+
+
+def _get_wikififier_concepts(resp):
+    annotations = [{URL_FIELD: ann[URL_FIELD],
+                    COSINE_FIELD: ann[COSINE_FIELD],
+                    PAGERANK_FIELD: ann[PAGERANK_FIELD]} for ann in resp.get("annotations", [])]
+
+    return {
+        ANNOTATION_DATA_FIELD: annotations,
+        STATUS_FIELD: resp[STATUS_FIELD]
+    }
+
+
+def _get_wikifier_response(text, api_key, df_ignore, words_ignore):
+    params = {"text": text,
+              "userKey": api_key,
+              "nTopDfValuesToIgnore": df_ignore,
+              "nWordsToIgnoreFromList": words_ignore}
+    r = requests.post(_WIKIFIER_URL, params)
+    if r.status_code == 200:
+        resp = json.loads(r.content)
+        if ERROR_KEY in resp:
+            raise ValueError("error in response : {}".format(resp[ERROR_KEY]))
+        return resp
+    else:
+        raise ValueError("http status code 200 expected, got status code {} instead".format(r.status_code))
+
+
+def wikify(text, key, df_ignore, words_ignore):
+    try:
+        resp = _get_wikifier_response(text, key, df_ignore, words_ignore)
+        resp[STATUS_FIELD] = 'success'
+    except ValueError as e:
+        try:
+            STATUS_ = e.message
+        except:
+            STATUS_ = e.args[0]
+        return {
+            STATUS_FIELD: STATUS_
+        }
+    time.sleep(0.5)
+    return _get_wikififier_concepts(resp)
---
+++ context_agnostic_engagement/feature_extraction/wikipedia_based_features.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+
+from context_agnostic_engagement.feature_extraction.api_utils import wikify
+from context_agnostic_engagement.feature_extraction.text_utils import partition_text
+
+# values for Doc Frequency and Words to Ignore, more details about these variables
+# found at: http://www.wikifier.org/info.html
+DF_IGNORE_VAL = 50
+WORDS_IGNORE_VAL = 50
+
+
+def get_wikipedia_topic_features(text, api_key, chunk_size=5000):
+    text_partitions = partition_text(text, max_size=chunk_size)
+
+    enrichments = []
+    i = 1
+    for text_part in text_partitions:
+        temp_record = {}
+        annotations = wikify(text_part, api_key, DF_IGNORE_VAL, WORDS_IGNORE_VAL)
+        temp_record["part"] = i
+        temp_record["text"] = text_part
+        temp_record["annotations"] = annotations
+        enrichments.append(temp_record)
+        i += 1
+
+    return enrichments
+
+
+def get_ranked_topics(chunks, option, top_n):
+    chunks = list(chunks)
+
+    total_length = sum([len(part["text"]) for part in chunks])
+
+    records = defaultdict(list)
+    for part in chunks:
+        annotations = part["annotations"]["annotation_data"]
+        weight = len(part["text"])
+        norm = weight / total_length
+        for concept in annotations:
+            url = concept["url"]
+            val = concept.get(option, 0.)
+            records[url].append(val * norm)
+
+    rec = [(title, sum(val)) for title, val in records.items()]
+
+    # sort by normalised weight
+    rec.sort(key=lambda l: l[1], reverse=True)
+    n_recs = rec[:top_n]
+
+    final_rec = {}
+    for idx, item in enumerate(n_recs):
+        url, val = item
+        _idx = idx + 1
+        final_rec["topic_{}_{}_url".format(_idx, option)] = url
+        final_rec["topic_{}_{}_val".format(_idx, option)] = val
+
+    return final_rec
+
+
+def get_authority_wiki_features(text, api_key, top_n):
+    # enrich with wikifier api
+    enriched_chunks = get_wikipedia_topic_features(text, api_key)
+    ranked_topic_records = get_ranked_topics(enriched_chunks, "pageRank", top_n)
+
+    return ranked_topic_records
+
+
+def get_coverage_wiki_features(text, api_key, top_n):
+    # enrich with wikifier api
+    enriched_chunks = get_wikipedia_topic_features(text, api_key)
+    ranked_topic_records = get_ranked_topics(enriched_chunks, "cosine", top_n)
+
+    return ranked_topic_records
