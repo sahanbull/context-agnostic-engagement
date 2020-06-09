c32ff839fb6b50f16474106a28930da83110177b
Sahan Bulathwela <sahabull@gmail.com>
2020-06-09 17:13:16 +0100

    feat: added docstrings for evaluation metrics

--- context_agnostic_engagement/helper_tools/evaluation_metrics.py
+++ context_agnostic_engagement/helper_tools/evaluation_metrics.py
@@ -6,6 +6,19 @@ from context_agnostic_engagement.helper_tools.io_utils import get_pairwise_versi
 
 
 def get_rmse(Y_train, Y_test, train_pred, test_pred):
+    """ calculates Root Mean Squared Error (RMSE) for train and test data
+
+    Args:
+        Y_train ([float]): actual label in training data
+        Y_test ([float]): actual label in testing data
+        train_pred ([float]): predicted label for training data
+        test_pred ([float]): predicted label for testing data
+
+    Returns:
+        train_rmse (float): metric for training data
+        test_rmse (float): metric for testing data
+
+    """
     train_rmse = np.sqrt(skm.mean_squared_error(Y_train, train_pred))
     test_rmse = np.sqrt(skm.mean_squared_error(Y_test, test_pred))
 
@@ -13,6 +26,19 @@ def get_rmse(Y_train, Y_test, train_pred, test_pred):
 
 
 def get_spearman_r(Y_train, Y_test, train_pred, test_pred):
+    """ calculates Spearman's Rank Order Correlation (SROCC) for train and test data
+
+    Args:
+        Y_train ([float]): actual label in training data
+        Y_test ([float]): actual label in testing data
+        train_pred ([float]): predicted label for training data
+        test_pred ([float]): predicted label for testing data
+
+    Returns:
+        train_spearman (float, float): r value and pvalue metric for training data
+        test_spearman (float, float): r value and pvalue metric metric for testing data
+
+    """
     train_spearman = spearmanr(Y_train, train_pred)
     test_spearman = spearmanr(Y_test, test_pred)
 
@@ -20,6 +46,22 @@ def get_spearman_r(Y_train, Y_test, train_pred, test_pred):
 
 
 def get_pairwise_accuracy(spark, label, fold_train_df, fold_test_df, train_pred, test_pred):
+    """calculates Pairwise Accuracy (Pairwise) for train and test data
+
+    Args:
+        spark (pyspar.SparkSession): spark session from Apache Spark
+        label (str): label (median, mean etc.)
+        fold_train_df (pd.DataFrame): training fold pandas DataFrame
+        fold_test_df (pd.DataFrame): testing fold pandas DataFrame
+        train_pred ([float]): predicted labels on training data
+        test_pred ([float]): predicted labels on testing data
+        test_pred ([float]): predicted labels on testing data
+
+    Returns:
+        train_accuracy (float): pairwise accuracy metric for training data
+        test_accuracy (float): pairwise accuracy metric metric for testing data
+
+    """
     train_Y_p = spark.createDataFrame(fold_train_df)
     train_Y_p = get_pairwise_version(train_Y_p, is_gap=True, label_only=True).toPandas()[
         [ID_COL, "_" + ID_COL, "gap_" + label, DOMAIN_COL]]
