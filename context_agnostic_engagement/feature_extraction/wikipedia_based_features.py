1199ca57779e66cb874a4c940db59d7e4cf64ac0
Sahan Bulathwela <sahabull@gmail.com>
2020-06-09 09:42:03 +0100

    feat: added all the doctrings for wiki features

--- context_agnostic_engagement/feature_extraction/wikipedia_based_features.py
+++ context_agnostic_engagement/feature_extraction/wikipedia_based_features.py
@@ -10,6 +10,17 @@ WORDS_IGNORE_VAL = 50
 
 
 def get_wikipedia_topic_features(text, api_key, chunk_size=5000):
+    """ get Wikification for the transcript using http://www.wikifier.org
+
+    Args:
+        text (str): text that needs to be Wikified
+        api_key (str): API key for Wikifier obtained from http://www.wikifier.org/register.html
+        chunk_size (int): maximum number of characters that need included in each Wikified fragment.
+
+    Returns:
+        enrichments ([{str: val}]): list of annotated chunks from the transcript
+
+    """
     text_partitions = partition_text(text, max_size=chunk_size)
 
     enrichments = []
@@ -27,6 +38,17 @@ def get_wikipedia_topic_features(text, api_key, chunk_size=5000):
 
 
 def get_ranked_topics(chunks, option, top_n):
+    """ ranks the topics using the aggregated score across multiple Wikified chunks of the text.
+
+    Args:
+        chunks ([{str: val}]): list of Wikified chunks for the transcript
+        option {str}: pageRank or cosine
+        top_n (int): n top ranked topics of interest
+
+    Returns:
+        final_rec ({str:val}): dict with key for top_n_url or top_n_value and the URL or value of the topic
+
+    """
     chunks = list(chunks)
 
     total_length = sum([len(part["text"]) for part in chunks])
@@ -58,7 +80,18 @@ def get_ranked_topics(chunks, option, top_n):
 
 
 def get_authority_wiki_features(text, api_key, top_n):
-    # enrich with wikifier api
+    """ returns top-n most authoritative Wikipedia topics with PageRank scores.
+    Calculated using http://www.wikifier.org/
+
+    Args:
+        text (str): text that needs to be Wikified for authority
+        api_key (str): API key for Wikifier obtained from http://www.wikifier.org/register.html
+        top_n (int): n top ranking topics to be returned with PageRank scores
+
+    Returns:
+        ranked_topic_records ({str:val}): dict with key for top_n_url or top_n_value and the URL or value of the topic
+
+    """
     enriched_chunks = get_wikipedia_topic_features(text, api_key)
     ranked_topic_records = get_ranked_topics(enriched_chunks, "pageRank", top_n)
 
@@ -66,7 +99,18 @@ def get_authority_wiki_features(text, api_key, top_n):
 
 
 def get_coverage_wiki_features(text, api_key, top_n):
-    # enrich with wikifier api
+    """ returns top-n most covered Wikipedia topics with cosine similarity scores.
+    Calculated using http://www.wikifier.org/
+
+    Args:
+        text (str): text that needs to be Wikified for coverage
+        api_key (str): API key for Wikifier obtained from http://www.wikifier.org/register.html
+        top_n (int): n top ranking topics to be returned with cosine scores
+
+    Returns:
+        ranked_topic_records ({str:val}): dict with key for top_n_url or top_n_value and the URL or value of the topic
+
+    """
     enriched_chunks = get_wikipedia_topic_features(text, api_key)
     ranked_topic_records = get_ranked_topics(enriched_chunks, "cosine", top_n)
 
